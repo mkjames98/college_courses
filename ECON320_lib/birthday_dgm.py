4a01e63438fce8c62326ed5f12de265bae80c3e0
mkjames98 <mkjames98@gmail.com>
2022-11-03 20:27:47 -0400

    assignemnt 8 push

--- ECON320_lib/birthday_dgm.py
+++ ECON320_lib/birthday_dgm.py
@@ -1,6 +1,7 @@
 import math
 
 import pandas as pd
+import numpy as np
 
 # function that finds the variance using my birthday,
 # note that "scale" parameter in rng.normal is standard deviation
@@ -15,7 +16,6 @@ def birthday_str_variance_srm(birthday_string):
             var_u = int(revb)
             break
     return var_x, var_u
-
 
 def generate_srm_birthday(
     birthday_string, rng, x_mean, u_mean, size
@@ -58,7 +58,6 @@ def birthday_str_variance_mrm(birthday_string):
     var_x2 = int(birthday_string[0]) + int(birthday_string[1])
     return var_x1, var_x2, var_u
 
-
 # refactored for mrm
 def generate_mrm_birthday(
     birthday_string, rng, x1_mean, x2_mean, u_mean, size
@@ -88,6 +87,48 @@ def generate_mrm_birthday(
         f"u is normally distributed with a mean of {u_mean} and variance of {round(var_u,3)}"
     )
 
+    y = pop_beta0 + pop_beta1 * x_1 + pop_beta2 * x_2 + u
+    print(f"y = {int(pop_beta0)} + {int(pop_beta1)}*x_1 + {int(pop_beta2)}*x_2 + u ")
+    gdata = {"x_1": x_1, "x_2": x_2, "u": u, "y": y}
+    df = pd.DataFrame(data=gdata)
+    return df, pop_beta0, pop_beta1, pop_beta2
+
+def generate_mrm_dynamic_birthday(
+    birthday_string, rng, x1_mean, u_mean, x2_alpha0, x2_alpha1, x2_resid_mean, x2_resid_var, size
+):  # x_mean = 10, u_mean =0
+    # beta0 is addition of 3rd and 4th digit of my birthday
+    pop_beta0 = int(birthday_string[2]) + int(birthday_string[3])
+    # beta1 is addition of 1st and 2nd digit of my birthday
+    pop_beta1 = int(birthday_string[0]) + int(birthday_string[1])
+    # beta2 is addition of 5th and 6th digit of my birthday
+    pop_beta2 = int(birthday_string[4]) + int(birthday_string[5])
+
+    # set scale, scale should be std, so sqrt the result
+    var_x1, var_u = birthday_str_variance_srm(birthday_string)
+    scale_x1 = math.sqrt(var_x1)
+    scale_u = math.sqrt(var_u)
+
+    x_1 = rng.normal(loc=x1_mean, scale=scale_x1, size=size)
+    print(
+        f"x_1 is normally distributed with a mean of {x1_mean} and variance of {round(var_x1,3)}"
+    )
+    u = rng.normal(loc=u_mean, scale=scale_u, size=size)
+
+    x_2 = []
+    scale_u2 = math.sqrt(x2_resid_var)
+    u_x2 = rng.normal(loc=x2_resid_mean, scale=scale_u2, size=size)
+    # x2i = alpha0 + alpha1 * x1i +ui
+    for i, x1_i in enumerate(x_1): # using enum to reference u_i's
+        x2_i = x2_alpha0 + x2_alpha1 * x1_i + u_x2[i]
+        x_2.append(x2_i)
+    x_2 = np.array(x_2)
+
+    print(
+        f"x_2 is normally distributed with a mean of {np.mean(x_2)} and variance of {round(np.var(x_2), 3)}"
+    )
+    print(
+        f"u is normally distributed with a mean of {u_mean} and variance of {round(var_u, 3)}"
+    )
     y = pop_beta0 + pop_beta1 * x_1 + pop_beta2 * x_2 + u
     print(f"y = {int(pop_beta0)} + {int(pop_beta1)}*x_1 + {int(pop_beta2)}*x_2 + u ")
     gdata = {"x_1": x_1, "x_2": x_2, "u": u, "y": y}
